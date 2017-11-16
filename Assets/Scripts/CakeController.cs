e1da86776b62ae6043746b2525fc3d8bdf4a2bd8
ksnake1 <kyungneung.lee@georgebrown.ca>
2017-11-16 11:25:55 -0500

    commit3

---
+++ Assets/Scripts/CakeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CakeController : MonoBehaviour {
+
+	//Public variables
+	[SerializeField]
+	private float speed = 3f;
+	[SerializeField]
+	private float startY;
+	[SerializeField]
+	private float endY;
+	[SerializeField]
+	private float startX;
+	[SerializeField]
+	private float endX;
+
+	//private variables
+	private Transform _transform;
+	private Vector2 _currentPos;
+
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.GetComponent<Transform> ();
+		_currentPos = _transform.position;
+		Reset ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_currentPos = _transform.position;
+		//move ocean down
+		_currentPos -= new Vector2 (speed, 0);
+
+		//check if we need to reset
+		if (_currentPos.x < endX) {
+			//reset
+			Reset ();
+		}
+		//apply changes
+		_transform.position = _currentPos;
+		
+	}
+
+	private void Reset(){
+
+		float y = Random.Range (startY, endY);
+		float dx = Random.Range (0, 300);
+		_currentPos = new Vector2 (startX + dx, y);
+	}
+}
---
+++ Assets/Scripts/MouseCollision.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCollision : MonoBehaviour {
+
+	//[SerializeField]
+	//GameController gameController;
+
+	[SerializeField]
+	GameObject explosion;
+
+
+	private AudioSource _islandSound;
+
+	// Use this for initialization
+	void Start () {
+		_islandSound = gameObject.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter2D(Collider2D other){
+
+
+		if (other.gameObject.tag.Equals ("island")) {
+			Debug.Log ("Collision island\n");
+			if (_islandSound != null) {
+				_islandSound.Play ();
+			}
+			//Add points
+			//Player.Instance.Score+=100;
+		}else if(other.gameObject.tag.Equals ("enemy")){
+			Debug.Log ("Collision enemy\n");
+
+			Instantiate (explosion).GetComponent<Transform> ().position = other.gameObject.GetComponent<Transform> ().position;
+
+			//other.gameObject.GetComponent<RocketController> ().Reset ();
+			//Player.Instance.Life-=1;
+
+			StartCoroutine( "Blink");
+		}
+
+	}
+
+	private IEnumerator Blink(){
+
+		Color c;
+		Renderer renderer = 
+			gameObject.GetComponent<Renderer> ();
+		for (int i = 0; i < 3; i++) {
+			for (float f = 1f; f >= 0; f -= 0.1f) {
+				c = renderer.material.color;
+				c.a = f;
+				renderer.material.color = c;
+				yield return new WaitForSeconds (.1f);
+			}
+			for (float f = 0f; f <= 1; f += 0.1f) {
+				c = renderer.material.color;
+				c.a = f;
+				renderer.material.color = c;
+				yield return new WaitForSeconds (.1f);
+			}
+		}
+	}
+}
